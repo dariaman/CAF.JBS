ff0fb61c0d9baa1971cbf937bae9408deb53aecd
dariaman <dariaman.siagian@gmail.com>
2017-05-13 08:54:29 +0700

    edit user entity

--- src/CAF.JBS/Startup.cs
+++ src/CAF.JBS/Startup.cs
@@ -66,7 +66,7 @@ namespace CAF.JBS
                     // User settings
                     options.User.RequireUniqueEmail = true;
                 })
-                .AddEntityFrameworkStores<JbsDbContext>()
+                .AddEntityFrameworkStores<UserDbContext>()
                 .AddDefaultTokenProviders();
             
             services.AddMvcCore().AddViewLocalization();
